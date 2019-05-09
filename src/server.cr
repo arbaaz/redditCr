4a4f1336324317725a2a7ec4bd5a8f199b6e137f
arbaaz <abdularbaaz@gmail.com>
2019-05-09 23:36:07 +0530

    add redirection

--- src/server.cr
+++ src/server.cr
@@ -5,7 +5,12 @@ require "./response"
 get "/:subreddit" do |env|
   env.response.headers["Access-Control-Allow-Origin"] = "*"
   subreddit = env.params.url["subreddit"]
-  response = HTTP::Client.get "https://www.reddit.com/r/#{subreddit}.json"
+  url = "https://www.reddit.com/r/#{subreddit}.json"
+  response = HTTP::Client.get url
+  if response.status_code == 302
+    url = response.headers["location"]
+    response = HTTP::Client.get url
+  end
   parsed_response = Response.from_json(response.body)
   parsed_response.to_json
 end
