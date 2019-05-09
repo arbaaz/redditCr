ea0fb3865e06920c63fa0f88023be3e708def108
arbaaz <abdularbaaz@gmail.com>
2019-05-10 00:22:44 +0530

    update json parsing

--- spec/post_list_spec.cr
+++ spec/post_list_spec.cr
@@ -1,11 +1,12 @@
 require "./spec_helper"
 
-describe PostLIst do
+describe PostList do
   string_or_io = File.read("spec/fixtures/post_list.json")
   pull = JSON::PullParser.new(string_or_io)
-  r = PostLIst.from_json(pull)
+  r = PostList.from_json(pull)
 
   it "id should be string" do
+    puts r.to_json
     r.map do |item|
       item.id.should be_a(String)
     end
--- src/post_list.cr
+++ src/post_list.cr
@@ -12,19 +12,21 @@ module PostList
   end
 
   def self.to_json(post_list, json)
-    json.start_array
-    post_list.each do |post|
-      json.object do
-        json.field "id", post.id
-        json.field "ups", post.ups
-        json.field "title", post.title
-        json.field "url", post.url
-        json.field "permalink", post.permalink
-        json.field "preview", post.preview
-        json.field "post_hint", post.post_hint
-        json.field "media_embed", post.media_embed
-      end
-    end
-    json.end_array
+    json.raw post_list.to_json
+    # alternative way of building json
+    # json.start_array
+    # post_list.each do |post|
+    #   json.object do
+    #     json.field "id", post.id
+    #     json.field "ups", post.ups
+    #     json.field "title", post.title
+    #     json.field "url", post.url
+    #     json.field "permalink", post.permalink
+    #     json.field "preview", post.preview
+    #     json.field "post_hint", post.post_hint
+    #     json.field "media_embed", post.media_embed
+    #   end
+    # end
+    # json.end_array
   end
 end
