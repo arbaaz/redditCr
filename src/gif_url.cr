178ac1f27f469d624088a912bba0e4027ace0b88
arbaaz <abdularbaaz@gmail.com>
2019-05-18 17:49:19 +0530

    add gif

---
+++ src/gif_url.cr
@@ -0,0 +1,34 @@
+require "json"
+require "html"
+
+module GifUrl
+  def self.from_json(pull)
+    result = ""
+    begin
+      pull.on_key!("images") do
+        pull.read_array do
+          pull.on_key!("source") do
+            pull.on_key!("variants") do
+              pull.on_key!("gif") do
+                pull.on_key!("source") do
+                  pull.on_key!("url") do
+                    result = pull.read_string
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    rescue exception
+      puts "Found exception:"
+      puts exception
+      "https://media.giphy.com/media/xTiN0L7EW5trfOvEk0/giphy.gif"
+    end
+    HTML.unescape(result)
+  end
+
+  def self.to_json(_media_embed, json)
+    json.string(_media_embed)
+  end
+end
