8cdd03e90a124e54bd9b8469c2f0c5a0843ea5fa
arbaaz <abdularbaaz@gmail.com>
2019-05-19 15:08:06 +0530

    --wip-- [skip ci]

--- src/post.cr
+++ src/post.cr
@@ -1,6 +1,22 @@
 require "json"
+require "html"
 require "./image_url"
 require "./gif_url"
+
+module Media
+  def self.from_json(pull)
+    begin
+      pull.on_key!("content") do
+        HTML.unescape(pull.read_string)
+      end
+    rescue exception
+    end
+  end
+
+  def self.to_json(_media, json)
+    json.string(_media)
+  end
+end
 
 class Post
   JSON.mapping(
@@ -10,6 +26,7 @@ class Post
     url: String?,
     permalink: String?,
     post_hint: String?,
+    media_embed: {type: String?, converter: Media},
     preview: {type: String?, converter: ImageUrl},
       # preview: {type: String?, converter: GifUrl}
 )
